83f4cd56485fecd4a5a56a925d95c3996e9e1259
Daniel Ryan <83863427+FierceBubble@users.noreply.github.com>
2023-01-05 02:32:50 +0700

    Fix small bug on compileSdkVersion

--- lib/widget/record_user_detail_list.dart
+++ lib/widget/record_user_detail_list.dart
@@ -3,6 +3,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../model/user.dart';
 
